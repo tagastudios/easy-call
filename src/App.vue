82f0e0d0d504011834a16ff180caf5c71c0cb87e
Taga Studios <tagastudios@gmail.com>
2022-10-27 18:10:05 -0400

    audio port in and music player func

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,48 @@
 <script setup>
-import LanguageSelector from "./components/LanguageSelector.vue";
-import ScreeningQuestions from "./components/ScreeningQuestions.vue";
-import CallCounter from "./components/CallCounter.vue";
-import { PrismaClient } from "@prisma/client";
+	import LanguageSelector from "./components/LanguageSelector.vue";
+	import ScreeningQuestions from "./components/ScreeningQuestions.vue";
+	import CallCounter from "./components/CallCounter.vue";
+	import MusicPlayer from "./components/MusicPlayer.vue";
+	// import { PrismaClient } from "@prisma/client";
+	import { ref } from "vue";
 
-import { ref } from "vue";
+	const lang = ref("eng");
+	const callCount = ref(0);
+	const goal = 250;
 
-const lang = ref("eng");
-const callCount = ref(0);
-const goal = 250;
+	// const prisma = new PrismaClient();
 
-// const prisma = new PrismaClient();
+	const questions = {
+		esp: [
+			"Estaba haciendo una encuesta a ver si me ayuda?",
+			"Cual es su principal preocupacion",
+			"Yadira Caraveo",
+			"Jena Griswald",
+		],
+		eng: [
+			`I'm doing a short survey, can you help?`,
+			`What's the main concern?`,
+			"Yadira Caraveo",
+			"Jena Griswald",
+		],
+	};
 
-const questions = {
-	esp: [
-		"Estaba haciendo una encuesta a ver si me ayuda?",
-		"Cual es su principal preocupacion",
-		"Yadira Caraveo",
-		"Jena Griswald",
-	],
-	eng: [
-		`I'm doing a short survey, can you help?`,
-		`What's the main concern?`,
-		"Yadira Caraveo",
-		"Jena Griswald",
-	],
-};
+	const answers = {
+		esp: {
+			agradecido: "./assets/audio/eng/espA1.m4a",
+			penoso: `Ohh lamento escuchar eso`,
+			"de acuerdo": "Entiendo perfectamente",
+		},
+		eng: {
+			thankfull: "Thank you!!",
+			petty: `Ohh I'm sorry to hear that`,
+			aggreable: "I totally understand",
+		},
+	};
 
-const answers = {
-	esp: {
-		agradecido: "Gracias!!",
-		penoso: `Ohh lamento escuchar eso`,
-		"de acuerdo": "Entiendo perfectamente",
-	},
-	eng: {
-		thankfull: "Thank you!!",
-		petty: `Ohh I'm sorry to hear that`,
-		aggreable: "I totally understand",
-	},
-};
-
-const nextCall = () => {
-	callCount.value++;
-};
+	const nextCall = () => {
+		callCount.value++;
+	};
 </script>
 
 <template>
@@ -51,6 +51,7 @@ const nextCall = () => {
 		<LanguageSelector :lang="lang" @update-lang="lang = $event" />
 		<CallCounter :goal="goal" :count="callCount" :lang="lang" />
 	</header>
+	<MusicPlayer />
 	<main>
 		<ScreeningQuestions
 			:questions="questions[lang]"
@@ -62,60 +63,60 @@ const nextCall = () => {
 </template>
 
 <style>
-.flex {
-	display: flex;
-}
-.align-center {
-	align-items: center;
-}
-.px-1 {
-	padding-inline: 0.5rem;
-}
-.ml-half {
-	margin-left: 0.5rem;
-}
-.ml-1 {
-	margin-left: 1rem;
-}
-.ml-2 {
-	margin-left: 2rem;
-}
-.mr-half {
-	margin-right: 0.5rem;
-}
-.mr-1 {
-	margin-right: 1rem;
-}
-.mr-2 {
-	margin-right: 2rem;
-}
-.mb-1 {
-	margin-bottom: 1rem;
-}
-.mb-2 {
-	margin-bottom: 2rem;
-}
-.mb-3 {
-	margin-bottom: 3rem;
-}
-.gap-1 {
-	gap: 0.5rem;
-}
-.lang-selector {
-	font-weight: 700;
-}
-.active {
-	color: rgb(158, 91, 224);
-}
-.icon {
-	font-size: 2rem;
-	cursor: pointer;
-	transition: all 0.25s ease-in-out;
-}
-.icon:hover {
-	color: rebeccapurple;
-}
-.icon:active {
-	color: rgb(158, 91, 224);
-}
+	.flex {
+		display: flex;
+	}
+	.align-center {
+		align-items: center;
+	}
+	.px-1 {
+		padding-inline: 0.5rem;
+	}
+	.ml-half {
+		margin-left: 0.5rem;
+	}
+	.ml-1 {
+		margin-left: 1rem;
+	}
+	.ml-2 {
+		margin-left: 2rem;
+	}
+	.mr-half {
+		margin-right: 0.5rem;
+	}
+	.mr-1 {
+		margin-right: 1rem;
+	}
+	.mr-2 {
+		margin-right: 2rem;
+	}
+	.mb-1 {
+		margin-bottom: 1rem;
+	}
+	.mb-2 {
+		margin-bottom: 2rem;
+	}
+	.mb-3 {
+		margin-bottom: 3rem;
+	}
+	.gap-1 {
+		gap: 0.5rem;
+	}
+	.lang-selector {
+		font-weight: 700;
+	}
+	.active {
+		color: rgb(158, 91, 224);
+	}
+	.icon {
+		font-size: 2rem;
+		cursor: pointer;
+		transition: all 0.25s ease-in-out;
+	}
+	.icon:hover {
+		color: rebeccapurple;
+	}
+	.icon:active {
+		color: rgb(158, 91, 224);
+	}
 </style>
---
+++ src/components/MusicPlayer.vue
@@ -0,0 +1,52 @@
+<template>
+	<button @click="isPlaying ? pausePlayer() : resumePlayer()">
+		{{ isPlaying ? "Pause" : "Play" }}
+	</button>
+</template>
+
+<script>
+	export default {
+		name: "MusicPlayer",
+		props: {
+			url: {
+				type: String,
+				// required: true,
+				default: "https://dl.espressif.com/dl/audio/ff-16b-2c-44100hz.m4a",
+			},
+			autoPlay: {
+				type: Boolean,
+				default: false,
+			},
+		},
+		data() {
+			return {
+				player: new Audio(),
+				isPlaying: false,
+				song: new URL("/src/assets/audio/eng/engQ1.m4a", import.meta.url),
+			};
+		},
+		mounted() {
+			console.log(this.song);
+			this.player.src = this.song;
+		},
+		methods: {
+			resumePlayer() {
+				this.player.play();
+				this.isPlaying = true;
+			},
+			pausePlayer() {
+				this.player.pause();
+				this.isPlaying = false;
+			},
+		},
+	};
+</script>
+
+<style scoped>
+	button {
+		border: rebeccapurple 2px solid;
+		border-radius: 15px;
+		padding: 0.25rem 0.5rem;
+		margin-bottom: 1rem;
+	}
+</style>
